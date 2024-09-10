b51cb471aee12f241f61765d5e170c00a98d6bde
Tarasenko Misha <misha0106@gmail.com>
2024-09-10 13:19:11 +0300

    first commit

---
+++ src/main/java/org/restaurant/flavorscape/FlavorscapeApplication.java
@@ -0,0 +1,13 @@
+package org.restaurant.flavorscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavorscapeApplication.class, args);
+    }
+
+}
